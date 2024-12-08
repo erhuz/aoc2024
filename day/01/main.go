349926c36864348c91b657b9f4022ba4f25e5151
Benjamin Fransson <benjamin.fransson@hey.com>
2024-12-08 01:41:17 +0100

    Day 1 - 1

--- day/01/main.go
+++ day/01/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
+	"sort"
+	"strconv"
 )
 
 func main() {
@@ -25,6 +28,40 @@ func main() {
 		return
 	}
 
-	// Print the content of the file
-	fmt.Println(string(content))
+	rowDelimiter := []byte("\n")
+	valueDelimiter := []byte("   ")
+
+	rows := bytes.Split(content, rowDelimiter)
+	rows = rows[:len(rows)-1]
+
+	leftList := []int{}
+	rightList := []int{}
+
+	for index := 0; index < len(rows); index++ {
+		values := bytes.Split(rows[index], valueDelimiter)
+
+		value1, err := strconv.Atoi(string(values[0]))
+		value2, err := strconv.Atoi(string(values[1]))
+		if err != nil {
+			fmt.Println("Couldn't convert int")
+		}
+
+		leftList = append(leftList, value1)
+		rightList = append(rightList, value2)
+	}
+
+	sort.Ints(leftList)
+	sort.Ints(rightList)
+
+	diff := 0
+	for index := 0; index < len(rows); index++ {
+		if leftList[index] > rightList[index] {
+			diff += leftList[index] - rightList[index]
+			fmt.Println(diff)
+		} else {
+			diff += rightList[index] - leftList[index]
+			fmt.Println(diff)
+		}
+	}
+	fmt.Println(diff)
 }
